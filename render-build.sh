ab7b3a7e503dfce3e187e03922d27ce124e0b0b5
evangtsystems <evangelos.lampos@gtsystems.gr>
2025-03-14 14:37:13 +0200

    :/:

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Installing Chrome..."
-curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
